73a45e339474a1e9b5fcabd69eb3c8d32391ff38
IBajadera <VDragon@tlen.pl>
2021-05-09 17:50:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function opinia(tresc){
+    console.log(document.getElementById("opinia").value);
+	if(tresc = "tak"){
+        console.log(tresc);
+  	    console.log("+1 byqu");
+    }
+    else{
+        console.log("Kobiety i lubiacy js'a nie mają głosu");
+    }
+}
